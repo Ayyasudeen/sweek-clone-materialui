d48f1d7f6688dac303a79617b3a07bc38d373878
Ayyasudeen <63138561+Ayyasudeen@users.noreply.github.com>
2021-10-13 17:53:00 +0530

    start using halfway done

--- src/components/Home/StartUsing.js
+++ src/components/Home/StartUsing.js
@@ -1,13 +1,29 @@
-import { Container, makeStyles } from "@material-ui/core";
+import { Button, Container, makeStyles } from "@material-ui/core";
 import React from "react";
+import { Colors } from "../../helpers/Colors";
 
 const useStyles = makeStyles({
   root: {
     backgroundImage: "url(/assets/images/start-using.jpg)",
-    height: "100vh",
+    height: "105vh",
+    objectFit: "contain",
+    backgroundPosition: "50% 50%",
   },
   container: {
     maxWidth: 1410,
+  },
+  button: {
+    borderRadius: 0,
+    backgroundColor: Colors.primary,
+    color: Colors.white,
+    fontSize: 24,
+    padding: "10px 65px",
+    marginTop: 15,
+  },
+  image: {
+    "& img": {
+      height: 38,
+    },
   },
 });
 
@@ -15,7 +31,17 @@ const StartUsing = () => {
   const classes = useStyles();
   return (
     <section className={classes.root}>
-      <Container maxWidth="false" className={classes.container}></Container>
+      <Container maxWidth="false" className={classes.container}>
+        <h2>START USING SWEEK NOW</h2>
+        <p>Join a global community of readers and writers</p>
+        <Button className={classes.button} variant="contained" disableElevation>
+          JOIN TODAY
+        </Button>
+        <div className={classes.image}>
+          <img src="/assets/images/googleplay.svg" alt="google-playstore" />
+          <img src="/assets/images/applestore.svg" alt="apple-playstore" />
+        </div>
+      </Container>
     </section>
   );
 };
